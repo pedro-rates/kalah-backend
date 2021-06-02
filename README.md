a7611875f28b6ade336612a708c05bd3e999a595
Pedro Rates <prrates@amazon.com>
2021-06-02 18:46:28 -0300

    Correcting some unit tests. Added README file

---
+++ README.md
@@ -0,0 +1,16 @@
+# KalahBakcend
+
+This repository hosts the code to the backend of my Kalah game implementation. It's written in Java and meant to be deployed as an AWS Lambda Function. It communicates with its front-end counterpart, available at https://github.com/pedro-rates/kalah-frontend
+
+
+## Build
+
+Run `gradle buildZip` to generate the zip archive containing all necessary files to be deployed as a Lambda function. The file will be generated on the `build\distributions` 
+directory. It's possible to upload the file to AWS Lambda using the AWS CLI or the management console.
+
+## Unit tests
+
+Run `gradle build` or `gradle test` to run the unit tests on the Board class.
+
+
+
--- src/test/java/kalahBackend/BoardTest.java
+++ src/test/java/kalahBackend/BoardTest.java
@@ -59,21 +59,22 @@ public class BoardTest {
 		board.setPits(pits);
 		int lastSeeededHouse = board.processSowing(0);
 		int[] reorganizedPits = board.getPits();
-		assertEquals(5, lastSeeededHouse);
-		assertEquals(reorganizedPits[1], 0);
-		assertEquals(reorganizedPits[5], 0);
-		assertEquals(reorganizedPits[12], 0);
-		assertEquals(reorganizedPits[6], 6);
+		assertEquals(4, lastSeeededHouse);
+		assertEquals(reorganizedPits[0], 0);
+		assertEquals(reorganizedPits[4], 0);
+		assertEquals(reorganizedPits[8], 0);
+		assertEquals(reorganizedPits[6], 8);
 		
 		pits = new int[]{5, 5, 5, 4, 4, 0, 1, 5, 5, 5, 4, 4, 0, 1};
+		// {0, 6, 6, 5, 5, 0, 7, 0, 5, 5, 4, 4, 0, 1};
 		board.setPits(pits);
 		lastSeeededHouse = board.processSowing(0);
 		reorganizedPits = board.getPits();
 		assertEquals(5, lastSeeededHouse);
-		assertEquals(reorganizedPits[1], 0);
+		assertEquals(reorganizedPits[0], 0);
 		assertEquals(reorganizedPits[5], 0);
-		assertEquals(reorganizedPits[12], 0);
-		assertEquals(reorganizedPits[6], 6);
+		assertEquals(reorganizedPits[7], 0);
+		assertEquals(reorganizedPits[6], 7);
 		
 		board.setCurrentTurn("PLAYER2");
 		pits = new int[]{2, 7, 0, 8, 6, 5, 1, 5, 4, 4, 4, 0, 0, 2};
